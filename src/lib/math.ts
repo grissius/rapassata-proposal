f78ac9a90f889f7992241e49748dc36e5c26ac31
Jaroslav Šmolík <grissius@gmail.com>
2019-03-18 13:58:55 +0100

    wip

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -5,58 +5,68 @@ const enum TomatoShape {
     Record = 'Record',
 }
 
-type Result<T, O> = O extends true ? T : T | undefined;
-interface Tomato<T, O extends boolean = false, S = TomatoShape.Atom> {
+interface ValidationResult<T> {
+    value: T;
+    pass: boolean;
+}
+
+interface Tomato<T, R extends boolean = false, S extends TomatoShape = TomatoShape.Atom> {
     type: string; // what type?
     required: boolean; // is required?
     default: T; // default value?
 
     // modeling methods
-    require: () => Tomato<T, true>; // make required
-    defaultTo: (defVal: T) => Tomato<T, true>; // set default value
-    validate: (fn: (val: T) => boolean | Promise<boolean>) => Tomato<T, O>; // add validation function
-    $: Result<T, O>; // finish construction of the attribute, type-cast it to wrong type to ensure correct types
+    require: () => Tomato<T, true, S>; // make required
+    defaultTo: (defVal: Values<T, T, S>) => Tomato<T, true, S>; // set default value
+    validate: (fn: (val: Values<T, T, S>) => boolean | Promise<boolean>) => Tomato<T, R, S>; // add validation function
 }
+
+// Tomato variances
+type AllTomatos<T = any, R extends boolean = any, S extends TomatoShape = any> = Tomato<T, R, S>;
 
 type StringTomato<T = string> = Tomato<T>;
 type NumberTomato<T = number> = Tomato<T>;
 type BooleanTomato<T = boolean> = Tomato<T>;
 type AnyTomato<T = any> = Tomato<T>;
-type ArrayTomato<T extends AnyTomato = AnyTomato> = Tomato<T, false, TomatoShape.Array>;
+type ArrayTomato<T extends AllTomatos = AnyTomato> = Tomato<T, false, TomatoShape.Array>;
 type ObjectTomato<T = any> = Tomato<T, false, TomatoShape.Object>;
-type RecordTomato<T extends AnyTomato = AnyTomato> = Tomato<T, false, TomatoShape.Record>;
+type RecordTomato<T extends Record<any, any>> = Tomato<T, false, TomatoShape.Record>;
 
-type UnwrapTomato<T> = T extends Tomato<infer I, true> ? I : T extends Tomato<infer I, false> ? I | undefined : T;
-type UnwrapTomatoShape<T> = T extends Tomato<infer T, infer O, infer S> ? S : T;
-
+// Builders
 interface Breeds {
-    string: Tomato<string>;
-    number: Tomato<number>;
+    string: StringTomato;
+    number: NumberTomato;
     boolean: Tomato<boolean>;
-    arrayOf: <T>(x: T) => Tomato<Array<T>>;
-    objectOf: <V>(val: V) => Tomato<{ [key: string]: V }>;
-    object: <V>(val: V) => Tomato<V>;
-    any: Tomato<any>;
+    array: <T extends AllTomatos>(x: T) => ArrayTomato<T>;
+    objectOf: <V>(val: V) => RecordTomato<Record<any, V>>;
+    object: <V>(val: V) => ObjectTomato<V>;
+    any: AnyTomato;
 }
 
-const { any, number, arrayOf, boolean, object, objectOf, string } = ('' as any) as Breeds;
-
-
+// Helpers
+// Return T or ?T based on the flag
+type ProcessRequired<T, R> = R extends true ? T : T | undefined;
+// Get value of a single tomato
+type UnwrapTomato<T> = T extends Tomato<infer I, infer R, infer S> ? ProcessRequired<I, R> : T;
+// Extract shape
+type UnwrapTomatoShape<T> = T extends Tomato<infer T, infer R, infer S> ? S : T;
+// Extract values from a complete tomato object
 type Values<T, U = UnwrapTomato<T>, S = UnwrapTomatoShape<T>> = S extends TomatoShape.Atom
     ? U
-    : S extends TomatoShape.Array
-    ? Array<UnwrapTomato<U>> // values?
-    : { [key in keyof NonNullable<U>]: Values<NonNullable<U>[key]> };
+    : S extends TomatoShape.Array ? Array<UnwrapTomato<U>>
+    : { [key in keyof U]: Values<U[key]> };
 
-type test = UnwrapTomato<StringTomato>;
+
+const { any, number, array, boolean, object, objectOf, string } = ('' as any) as Breeds;
+
+type x = UnwrapTomato<typeof string>
+const b = object({ o: string, r: string.require() });
+type B = Values<typeof b>;
 
 type A = Values<typeof string>;
 
-const b = object({ foo: string });
-type B = Values<typeof b>;
-
-const c = object({ foo: arrayOf(number) });
-// const c = arrayOf(number);
+const c = object({ foo: array(number) });
+// const c = array(number);
 type C = Values<typeof c>;
 
 const Nest = object({
@@ -65,7 +75,7 @@ const Nest = object({
         address: object({
             street: string.validate(x => x !== ''),
             number: string.require(),
-        }),
+        }).validate(x => ),
         s: string.defaultTo('red'),
     }),
 });
@@ -76,11 +86,11 @@ const Req = {
     user: {
         age: number.defaultTo(18),
         address: {
-            street: string.validate(x => x !== ''),
+            street: string.validate(x =>),
             number: string.require(),
         },
-        colors: arrayOf(string.defaultTo('red')).defaultTo([]),
-        ratedHobbies: objectOf(number).require(),
+        colors: array(string.defaultTo('red')).defaultTo([]).validate(x =>),
+        ratedHobbies: objectOf(number).require().validate(x => ),
     },
 };
 
